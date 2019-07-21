42ffab5bd7fc2552d4298383f7881651511645c5
Nicolef Avella <nicolef.avella@uptc.edu.co>
2019-07-21 17:39:54 -0500

    add login

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -12,6 +12,8 @@ from profiles_api import permissions
 
 from rest_framework import filters
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -99,5 +101,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
     filter_backends = (filters.SearchFilter,)
-    search_filter = ('name', 'email',)
+    search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    '''para login,  usuario autenticado token  '''
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
