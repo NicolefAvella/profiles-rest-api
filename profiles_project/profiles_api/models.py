9bc82dbec986657af75bfc92884d522d56de6c96
Nicolef Avella <nicolef.avella@uptc.edu.co>
2019-07-21 01:43:53 -0500

    Creacion modelo usuario

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager():
+    ''' administrador de perfiles de usuario '''
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("El usuario debe tener un email")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)  #metodo set_pass django encripta la contraseña
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_coordinator = True
+        user.save(using=self._db)
+
+        return user
+
 
 # Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    ''' para crear nuestro modelo de  usuario'''
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_coordinator = models.BooleanField(default=False) #es coordinador area
+    process = models.CharField(max_length=100) #proceso dentro de la empresa al que pertenece
+
+    objects = UserProfileManager()
+
+    #para que funcione con admin django
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -118,7 +118,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
