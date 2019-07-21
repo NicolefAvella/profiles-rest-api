e83d07f106622b40c390d9393c12bd128b3716e0
Nicolef Avella <nicolef.avella@uptc.edu.co>
2019-07-21 12:09:14 -0500

    Metodo get

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+    '''Test API View'''
+
+    def get(sel, request, format=None):
+        '''get retorna uno o mas items'''
+        an_apiview = ['Practica metodos HTTP']
+
+        return Response({'message':'Hola', 'an_apiview':an_apiview})
+#''' post crea un item'''
+#'''put actualiza un item'''
+#'''path actualiza parcialmente un item'''
+#'''delete borra item'''
